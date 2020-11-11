cf0feb5c94a6932baf3124c8a9fac4da69f42925
Edward-Leopold <73839778+Edward-Leopold@users.noreply.github.com>
2020-11-11 08:43:24 +0300

    добавление всех файлов

---
+++ script.js
@@ -0,0 +1,52 @@
+$(function() {
+	let header = $("#header");
+	let intro = $("#intro");
+	let nav = $("#nav");
+	let navToggle = $("#navToggle");
+	let introH = intro.innerHeight();
+	let scrollPos = $(window).scrollTop();
+
+	checkScroll(scrollPos, introH);
+
+		$(window).on("scroll resize", function() {
+		let introH = intro.innerHeight();
+		scrollPos = $(this).scrollTop();
+
+		checkScroll(scrollPos, introH);
+	});
+
+
+	function checkScroll(scrollPos, introH){
+		if ( scrollPos > introH) {
+			header.addClass("fixed");
+		} else {
+			header.removeClass("fixed");
+		}
+	}
+	
+	$("[data-scroll]").on("click", function(event) {
+			event.preventDefault();
+
+			let elementID = $(this).data("scroll");
+			let elementOffset = $(elementID).offset().top;
+
+			nav.removeClass("show");
+
+			$("html, body").animate({
+				scrollTop: elementOffset - 55
+			}, 700);
+		});
+
+
+	/*Nav Toggle*/
+
+	$("#navToggle").on("click", function(event) {
+		event.preventDefault();
+
+		nav.toggleClass("show");
+	});
+
+
+
+
+});
